863fa97202daa2e267e94a157f4e55400fa4654b
Olga Nadenenko <slihelly@gmail.com>
2022-11-06 01:16:08 +0400

    LeetCode - 258. Add Digits

---
+++ src/main/java/com/oan/leet/AddDigitsSolution.java
@@ -0,0 +1,10 @@
+package com.oan.leet;
+
+public class AddDigitsSolution {
+    public int addDigits(int num) {
+        if(num == 0) {
+            return 0;
+        }
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+}
---
+++ src/test/java/com/oan/utils/leet/AddDigitsTest.java
@@ -0,0 +1,25 @@
+package com.oan.utils.leet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.oan.leet.AddDigitsSolution;
+import org.junit.Test;
+
+public class AddDigitsTest {
+    AddDigitsSolution solution = new AddDigitsSolution();
+
+    @Test
+    public void solutionTest1() {
+        assertThat(solution.addDigits(38)).isEqualTo(2);
+    }
+
+    @Test
+    public void solutionTest2() {
+        assertThat(solution.addDigits(0)).isEqualTo(0);
+    }
+
+    @Test
+    public void solutionTest3() {
+        assertThat(solution.addDigits(2147483647)).isEqualTo(1);
+    }
+}
